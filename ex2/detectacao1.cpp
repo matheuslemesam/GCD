ad8945a94790eb3588c3caf78f071dcb621fb04d
thlemez <matheuslemesamgmail.com>
2024-12-21 22:46:54 -0300

    Detectacao1 com bugs, detectacao 2 completo.

---
+++ ex2/detectacao1.cpp
@@ -0,0 +1,51 @@
+//Lógica 1, utilizar HSV para detectar o raio luminoso pela cor com base no seu intervalo de cores.
+
+// g++ detectacao1.cpp -o main.exe `pkg-config --cflags --libs opencv4`
+
+#include <opencv2/opencv.hpp> // incluir OpenCV
+#include <stdio.h> // incluir lib padrão
+
+int main() {
+
+    using namespace cv; // namespace para não precisar ficar repetindo cv::
+
+    Mat luz = imread("images/luz.jpg"); // carregar a imagem da luz visível
+    if (luz.empty()) { //condição para detectar se a imagem foi carregada/ se o caminho está
+        printf("Erro ao carregar a imagem!\n"); 
+        return -1;
+    }
+
+    // Converter a imagem para o espaço de cores HSV
+    Mat hsv;
+    cvtColor(luz, hsv, COLOR_BGR2HSV);
+
+    // Definir os limites da cor azul em HSV
+    Scalar menorazul(126, 0, 100);   
+    Scalar maiorazul(150, 255, 255);
+    Scalar branco(0, 0, 200);
+    Scalar branco2(179, 55, 255);
+    Scalar roxo(151, 0, 100);
+    Scalar roxo2(169, 255, 255);
+
+    // Criar máscaras para as duas faixas de vermelho
+    Mat mascara1, mascara2, mascara3;
+    inRange(hsv, menorazul, maiorazul, mascara1);
+    inRange(hsv, branco, branco2, mascara2);
+    inRange(hsv, roxo, roxo2, mascara3);
+
+    // Combinar as duas máscaras
+    Mat mascara = mascara1 | mascara2 | mascara3;
+
+
+    // Aplicar a máscara à imagem original
+    Mat resultado;
+    bitwise_and(luz, luz, resultado, mascara);
+
+    // Mostrar as imagens
+    imshow("Imagem Original", luz);
+    //imshow("Máscara", mascara);
+    imshow("Resultado", resultado);
+
+    waitKey(0);
+    return 0;
+}
---
+++ ex2/detectacao2.cpp
@@ -0,0 +1,40 @@
+//Lógica 2, transformar a imagem em preto e branco e aplicar a máscara.
+
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
+
+int main() {
+    
+    using namespace cv; // namespace para não precisar ficar repetindo 
+
+    Mat image = imread("images/luz.jpg"); // Carregar a imagem
+
+    if (image.empty()) { // Verificar se a imagem foi carregada corretamente ou se o caminho está errado
+        printf("Erro ao carregar a imagem!\n"); // Usando fprintf para erro
+        return -1;
+    }
+
+    Mat peb; // converter a imagem para cinza
+    cvtColor(image, peb, COLOR_BGR2GRAY);
+
+    int minimo = 200; // Tons próximos ao branco
+    int maximo = 255; // Branco puro
+
+    Mat mascara;
+    inRange(peb, Scalar(minimo), Scalar(maximo), mascara);
+
+    // Aplicar a máscara à imagem original
+    Mat resultado;
+    bitwise_and(peb, peb, resultado, mascara);
+
+
+    imshow("Img Og", image); // mostrar a imagem original
+
+    imshow("Img cinza", resultado); // mostrar a imagem cinza
+
+    waitKey(0); // esperar até que uma tecla seja pressionada
+
+    destroyAllWindows(); // Fechar todas as janelas criadas com o imshow
+
+    return 0;
+}
