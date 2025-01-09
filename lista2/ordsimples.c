516f4aac933f45422ac9cc1f8daa5591910ec50e
thlemez <matheuslemesamgmail.com>
2025-01-08 21:46:13 -0300

    Anotações EDA2A1Pt2 + Código 1? (sem saída, erro do vscode??)

--- lista2/ordsimples.c
+++ lista2/ordsimples.c
@@ -1,51 +1,47 @@
-/*
-quest: ordenação simples
-https://moj.naquadah.com.br/contests/gcd_trainee_cdmoj2/moj-problems_ordenasimples.html
+#include <stdio.h>
 
-criar uma função que ordene um vetor de inteiros em ordem crescente. Utilize o algoritmo de ordenação por inserção ou seleção ou bolha.
+// Cabeçalhs
+void selection_sort(int numeros[], int l, int r);
 
-Estudos para o código:
-    Materiais:
-    https://www.youtube.com/watch?v=ZjzAZdcgc0k&list=PLzZut2slkqyyUnJfIf6Izjkr6HTmsplBG
+int less(int a, int b);
 
-    Ordenação:
-        Oque vamos ordenar? Arquivos de itens, vetores, listas. Dentro desses arquivos teremos os itens que queremos ordenar (chaves).
-            Ex: em uma lista de alunos, queremos ordenar por nome, matrícula, nota, etc.
+void exch(int a, int b);
 
-        Objetivo? Rearranjar as chaves de forma que fiquem em conformidade com a ordem estabelecida.
-        O arquivo cabe na memória? Sim, então podemos usar algoritmos de ordenação interna. Se não, usamos algoritmos de ordenação externa.
+int main (void){
 
-        Vetores ou listas encadeadas? Vetores são mais fáceis de implementar, mas listas encadeadas são mais flexíveis. Muda a complexidade dos algoritmos de acordo com o tipo de dado.
+    int vetor[999]; //já que o vetor pode ir só até 1000 elementos.
+    int i = 0;
 
-        typedef int item; // item é a variável do tipo inteiro
-        #define key(A) // se for um int retornará A, se for um struct retornará A.chave.
-        #define less(A, B) // se A é menor que B (Key(A)<Key(B)), retornará 1, senão 0.
-        #define exch(A, B) // troca os valores de A e B. Item temporário(t) = A; A = B; B = t;
-        #define cmpxch(A, B) // troca os valores de A e B, se B for menor que A. if(less(B, A)) exch(A, B);
+    while (scanf("%d", &vetor[i]) != EOF){
+        selection_sort(vetor, 0, i);
+        i++;
+    }
 
-        Algoritmos de Ordenação estáveis: itens com chaves idênticas não mudam de posição.
-            Ex: Ordenar por nota decrescente
-                    Ex base:                Se estável:             Se instável(ordem aleatória):
-                    Bruno 90                Bruno 90                Nina 90
-                    Rose 105                Nina 90                 Bruno 90
-                    Nina 90                 Rose 105                Rose 105
+    for (int j = 0; j < i; j++){
+        printf("%d ", vetor[j]);
+    }
 
-            Vantagens do estável? Preservação da ordem relativa, Aplicações específicas, Facilita ordenações múltiplas, Previsibilidade.
+    return 0;
+}
 
-            Vantages do instável? Mais eficiente, Menos complexo, Menos memória, Mais rápido.
+int less(int a, int b) { //função que verifica se a é menor que b.
+    return a < b;
+}
 
-        Minhas considerações: Para numeros com intervalos maiores como 0 a 100, a ordenação instável é melhor já que a probabilidade de terem números repetidos é menor e ela é mais eficiente.
+void exch(int a, int b) { //função que troca os valores de a e b.
+    int t = a;
+    a = b;
+    b = t;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-*/
+void selection_sort(int numeros[], int l, int r) { //vetor, item mais a esquerda e item mais a direita. 
+            for (int i = l; i < r; i++) { //i começa no item mais a esquerda e vai até o item mais a direita.
+            //Nesse caso, item l = 0 e item r = 4. 
+            int menor = i;
+            for (int j = i+1; j <= r; j++) { //j começa no próximo item a esquerda de l e vai até o item mais a direita.
+                if (less(numeros[j], numeros[menor])) menor = j; //se o item j for menor que o item menor, menor recebe j.
+            }
+            exch(numeros[l], numeros[menor]); //troca o valor de l p/ menor após ser verificado qual o menor.
+            selection_sort(numeros, l+1, r); //chama a função novamente, mas com l+1, ou seja, o próximo item a esquerda.
+            } 
+}
