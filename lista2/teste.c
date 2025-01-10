1afa8b9d98403523d5438b21b096ee6e0a63b8e4
thlemez <matheuslemesamgmail.com>
2025-01-10 19:55:47 -0300

    teste e algoritmo de selection sort com busca binária

---
+++ lista2/teste.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (){
+
+    int v[2] = {1, 4};
+
+    printf("printando v[0]: %d\n", v[0]); //Saída: 1 
+    printf("printando v[1]: %d\n", v[1]); //Saída: 4
+    printf("printando v[2]: %d\n", v[2]); //Saída: nmr aleatório
+
+    return 0;
+}
