960f644e218bd5a2c4d2ce8dbe5870033f8b8c7a
thlemez <matheuslemesamgmail.com>
2025-01-25 16:41:01 -0300

    Att square.cpp

--- pessoal/square.cpp
+++ pessoal/square.cpp
@@ -6,12 +6,20 @@ using namespace cv;
 using namespace std;
 
 int main() {
-    
+
     // carregar imagem convertida para escala de cinza, aplicar blur e afiar a imagem
-    Mat image = imread("square.png",  IMREAD_GRAYSCALE);
+    Mat image = imread("images/square.png",  IMREAD_GRAYSCALE);
+
+    // debug img
+    if (image.empty()) { //condição para detectar se a imagem foi carregada/ se o caminho está
+        printf("Erro ao carregar a imagem!\n"); 
+        return -1;
+    }
+
+    // Aplicar blur e sharpen (suavizar e afiar a imagem)
     Mat blur, sharpen;
-    medianBlur(image, blur, 5);
-    Mat sharpen_kernel = (Mat_<float>(3,3) << -1,-1,-1, -1,9,-1, -1,-1,-1);
+    medianBlur(image, blur, 5); //reduz ruído enquanto preserva bordas; kernel de tam 5
+    Mat sharpen_kernel = (Mat_<float>(3,3) << -1,-1,-1, -1,9,-1, -1,-1,-1); //kernel para afiar
     filter2D(blur, sharpen, -1, sharpen_kernel);
 
     // Aplicar threshold e morfologia close
@@ -20,7 +28,7 @@ int main() {
     Mat kernel = getStructuringElement(MORPH_RECT, Size(3,3));
     morphologyEx(thresh, close, MORPH_CLOSE, kernel, Point(-1,-1), 2);
 
-    // Encontrar contornos e filtrar usando a área de threshold
+    // encontrar contornos e filtrar usando a área de threshold
     vector<vector<Point>> cnts;
     findContours(close, cnts, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
 
@@ -31,15 +39,15 @@ int main() {
         double area = contourArea(cnts[i]);
         if (area > min_area && area < max_area) {
             Rect rect = boundingRect(cnts[i]);
-            Mat ROI = image(rect);
-            imwrite("ROI_" + to_string(image_number) + ".png", ROI);
+            Mat ROI = image(rect); //Rectangle On Image
+            //imwrite("ROI_" + to_string(image_number) + ".png", ROI);
             rectangle(image, rect, Scalar(36, 255, 12), 2);
             image_number++;
         }
     }
 
     imshow("sharpen", sharpen);
-    imshow("close", close);
+    imshow("close", close); //imagem após aplicar morfologia, detecta contornos
     imshow("thresh", thresh);
     imshow("image", image);
     waitKey(0);
